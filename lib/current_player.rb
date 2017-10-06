fef03db3a16a15f84facf93178c2988b089b3a32
Jacqueline Lee <anginblu@gmail.com>
2017-10-06 18:50:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"])
   count = 0
   board.each do |position|
-    if position == "X" || position == "O"
+    if position == "X"
       count +=1
     end
   end
